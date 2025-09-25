81e9419456faac6f9e4bd93d8f4cb71d425b06a6
Sahil <sk950121@gmail.com>
2025-09-25 15:52:44 +0530

    Initial Commit

---
+++ tasveer/tasveerApp.swift
@@ -0,0 +1,17 @@
+//
+//  tasveerApp.swift
+//  tasveer
+//
+//  Created by S1 on 25/09/25.
+//
+
+import SwiftUI
+
+@main
+struct tasveerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
